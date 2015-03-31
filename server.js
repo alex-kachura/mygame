3399623fd7ad8e3a14496a85dc7e72775dd1d43e
alex-kachura <alex.kachura@gmail.com>
2015-03-31 13:44:00 +0200

    ready to deploy

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+
+if (env === 'development') {
+    mongoose.connect('mongodb://localhost/mygame');
+} else {
+    mongoose.connect('mongodb://alex:mygame@ds039311.mongolab.com:39311/mygame');
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('mygame db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
+app.get('/*', function(req, res, next){
+    res.setHeader('Last-Modified', (new Date()).toUTCString());
+    next();
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+
+console.log('Listening to port ' + port + '...');
