9d57c1043fa42df181829d8798f87d3513534ccf
alex-kachura <alex.kachura@gmail.com>
2015-04-08 12:05:04 +0200

    game page

--- server/config/express.js
+++ server/config/express.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-    logger = require('morgan'),
+    //logger = require('morgan'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
@@ -8,7 +8,7 @@ var express = require('express'),
 module.exports = function(app, config) {
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'jade');
-    app.use(logger('dev'));
+    //app.use(logger('dev'));
     app.use(cookieParser());
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
