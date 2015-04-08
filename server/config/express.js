aa577f6a55f0d7432f6faf7c6dfa6e4181e0a74a
alex-kachura <alex.kachura@gmail.com>
2015-04-08 12:07:32 +0200

    heroku fixes

--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     //logger = require('morgan'),
-    bodyParser = require('body-parser'),
+    //bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
     passport = require('passport');
@@ -10,8 +10,8 @@ module.exports = function(app, config) {
     app.set('view engine', 'jade');
     //app.use(logger('dev'));
     app.use(cookieParser());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
+    //app.use(bodyParser.urlencoded({extended: true}));
+    //app.use(bodyParser.json());
     app.use(session({secret: 'my game unicorns', resave: false, saveUninitialized: false}));
     app.use(passport.initialize());
     app.use(passport.session());
